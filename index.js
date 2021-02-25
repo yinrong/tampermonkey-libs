84267d5f2fa6bd5b6a7db39a94fb0b47df6c4b7a
yinrong <yinrong@xiaomi.com>
2021-02-25 15:40:53 +0800

    tlib: sleep,until,loadJquery,repeat,appendCss

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+;(async () => {
+    let $
+
+    window.tlib = {
+         sleep: (ms) => {
+             return new Promise(resolve => setTimeout(resolve, ms))
+         },
+         until: async (cond) => {
+             while (!cond()) {
+                 await tlib.sleep(1000)
+             }
+         },
+         loadJquery: async () => {
+             var jq = document.createElement('script')
+             jq.src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"
+             document.body.appendChild(jq)
+             await tlib.until(() => window.jQuery)
+             $ = window.jQuery
+             return window.jQuery
+         },
+         repeat: (func, interval=3000) => {
+             setInterval(func, interval)
+         },
+         appendCss: (s) => {
+             $('<style/>')
+             .appendTo($('body'))
+             .html(s)
+         },
+    }
+
+})();
